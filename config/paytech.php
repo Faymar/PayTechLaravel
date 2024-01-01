254e974b3e72b321291543d118acc70474057164
Faymar <97180375+Faymar@users.noreply.github.com>
2024-01-01 18:03:32 +0000

    firte commit

---
+++ config/paytech.php
@@ -0,0 +1,7 @@
+<?php
+
+// config for Babaly/LaravelPaytech
+return [
+    'PAYTECH_API_KEY' => '8cca77b4a640635101d66ed8ff824cdf589722b29bb104326c8e6e813fc826bf',
+    'PAYTECH_SECRET_KEY' => 'd5246d3dacaf6aee14450013c5a7a9b7ee493aa284822e58944fb7bdd0f3489c',
+];
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PaymentController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('payment', [PaymentController::class, 'index'])->name('payment.index');
+Route::post('/checkout', [PaymentController::class, 'payment'])->name('payment.submit');
+Route::get('ipn', [PaymentController::class, 'ipn'])->name('paytech-ipn');
+Route::get('payment-success/{code}', [PaymentController::class, 'success'])->name('payment.success');
+Route::get('payment/{code}/success', [PaymentController::class, 'paymentSuccessView'])->name('payment.success.view');
+Route::get('payment-cancel', [PaymentController::class, 'cancel'])->name('paytech.cancel');
